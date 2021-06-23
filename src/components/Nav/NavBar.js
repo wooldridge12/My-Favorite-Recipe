7cb5570ff882b7d839e6ec6b96a65309ba0defc5
wooldridge12 <wooldridgemikey@gmail.com>
2021-06-23 16:25:08 -0400

    Found code that got deleted in the process of pushing to github. Everything works now

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -6,10 +6,10 @@ export const NavBar = () => {
     return (
         <ul className="navbar">
             <li className="navBarItem">
-                <Link className="navbarLink" to="/posts">Main Page</Link>
+                <Link className="navBarLink" to="/posts">Main Page</Link>
             </li>
             <li className="navBarItem">
-                <Link className="navbarLink myCookingBookImage" to="/likes">My Cooking Book</Link>
+                <Link className="navBarLink myCookingBookImage" to="/likes">My Cooking Book</Link>
             </li>
             <li>
                 <Link className="navBarLink" to="/login" onClick={
